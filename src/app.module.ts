eadc67f67028b76a5a5c5158c654361ad555a75e
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-11-13 22:58:29 +0100

    Refactor configuration files: update nest-cli.json, package.json, and tsconfig.build.json for improved module paths and build options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { NatsModule } from './transports';
+import { NatsModule } from './transports/nats.module';
 import { ProductsModule } from './modules/products/products.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { AllergensModule } from './modules/allergens/allergens.module';
